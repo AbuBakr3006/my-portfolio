0cdf7d56b9310ed28d8ea23ca4ed4ff535fd9894
Abu Bakr Khabibullayev <abubakr3006@gmail.com>
2022-02-13 22:51:18 +0500

    commit

--- js/main.js
+++ js/main.js
@@ -1,11 +1,12 @@
-let elOpenModal = document.getElementById('btn')
-let elCloseModal = document.getElementById('close')
-let elModal = document.getElementById('md')
+let elOpenModal = document.getElementById('btn');
+let elCloseModal = document.getElementById('btn');
+let elModal = document.getElementById('md');
 
 elOpenModal.addEventListener('click', function(){
     elModal.classList.add('show');
 })
 
 elCloseModal.addEventListener('click', function (){
-    elModal.classList.remove('show');
+    elModal.classList.toggle('show__none');
 } )
+
